aef972ec2920e4776fd9fb5dc5f248e0a32ae83f
Chris Lusk <chris.m.lusk@gmail.com>
2018-09-14 12:34:19 -0500

    move repeated headings into seating chart

--- client/components/student-view.js
+++ client/components/student-view.js
@@ -17,11 +17,9 @@ export default class StudentView extends React.Component {
 
   render() {
     return (
-    <div className="mainContent">
+    <main className="mainContent">
       <h1 id="title">PAIR PROGRAMMING SEATING CHART</h1>
-      <h1 id="teacherDesks">Fellows' Desks</h1>
       <SeatingChart />
-      <h1 id="bathrooms">Bathrooms</h1>
-    </div>)
+    </main>)
   }
 };
